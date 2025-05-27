09f8c9b8a915a587611cd89ffc415bf7afd4b291
pandega <pandegaabyan@gmail.com>
2025-05-27 07:32:40 +0700

    refactor by changing table creation instead of new migration

--- runarion-laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ runarion-laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,8 +16,9 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->json('settings')->nullable();
             $table->string('avatar_url')->nullable();
+            $table->json('settings')->nullable();
+            $table->json('notifications')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- runarion-laravel/database/migrations/2025_03_30_010420_create_projects_table.php
+++ runarion-laravel/database/migrations/2025_03_30_010420_create_projects_table.php
@@ -16,9 +16,7 @@ return new class extends Migration {
             $table->foreignId('folder_id')->nullable()->constrained()->nullOnDelete();
             $table->string('name');
             $table->string('slug')->unique();
-            $table->text('description')->nullable();
             $table->json('settings')->nullable(); // For project-specific settings
-            $table->boolean('is_public')->default(false);
             $table->boolean('is_active')->default(true);
             $table->timestamps();
             $table->softDeletes();
