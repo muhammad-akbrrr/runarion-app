30691630c128a413cf15f060e40b84874552d63d
pandega <pandegaabyan@gmail.com>
2025-05-30 14:21:41 +0700

    update primary workspace id by allowing null

--- runarion-laravel/database/migrations/2025_03_30_010150_create_users_table.php
+++ runarion-laravel/database/migrations/2025_03_30_010150_create_users_table.php
@@ -17,13 +17,13 @@ return new class extends Migration {
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar_url')->nullable();
-            $table->ulid('primary_workspace_id');
+            $table->ulid('primary_workspace_id')->nullable();
             $table->json('settings')->nullable();
             $table->json('notifications')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
-            $table->foreign('primary_workspace_id')->references('id')->on('workspaces')->onDelete('cascade');
+            $table->foreign('primary_workspace_id')->references('id')->on('workspaces')->onDelete('set null');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
