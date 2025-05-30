45bf82d713c1ce98a20dcd027b080f5e1b6c766a
pandega <pandegaabyan@gmail.com>
2025-05-30 14:21:13 +0700

    remove unused imports

--- runarion-laravel/database/seeders/WorkspaceSeeder.php
+++ runarion-laravel/database/seeders/WorkspaceSeeder.php
@@ -4,9 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
 use App\Models\Workspace;
-use App\Models\WorkspaceMember;
 
 /**
  * Workspace Seeder
