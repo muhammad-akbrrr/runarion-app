7b62c92c246befb9b92ef3a33f6775dda1b00bce
Fadhil Umar Al Farouq <106678416+Alfadhils@users.noreply.github.com>
2025-06-03 16:48:54 +0700

    LLM Provider Initialization + Generation Endpoint (#32)
    * fix: update virtual environment activation path for Stable Diffusion setup
    
    * feat: implement base LLM generation API with OpenAI and Gemini providers
    
    * feat: refactor LLM generation API to use structured request models and improve error handling
    
    * refactor: cleaner implementation of LLM generation, implement instruction builder for story generation
    
    * feat: docker compatible, still require manual start
    
    * feat add to-be implemented quota manager, commented out for now
    
    * update root level .env.example, environment activation, and gitignore

---
+++ runarion-python/src/models/quota.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field
+from typing import Optional, Dict, Any
+from datetime import datetime
+
+# Not used for now
+class WorkspaceQuota(BaseModel):
+    id: int
+    name: str
+    slug: str
+    cover_image_url: Optional[str]
+    timezone: Optional[str]
+    settings: Optional[Dict[str, Any]]
+    permissions: Optional[Dict[str, Any]]
+    cloud_storage: Optional[Dict[str, Any]]
+    llm: Optional[Dict[str, Any]]
+    billing_email: Optional[str]
+    billing_name: Optional[str]
+    billing_address: Optional[str]
+    billing_city: Optional[str]
+    billing_state: Optional[str]
+    billing_postal_code: Optional[str]
+    billing_country: Optional[str]
+    billing_phone: Optional[str]
+    billing_tax_id: Optional[str]
+    stripe_customer_id: Optional[str]
+    stripe_subscription_id: Optional[str]
+    trial_ends_at: Optional[datetime]
+    subscription_ends_at: Optional[datetime]
+    is_active: bool = True
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+    deleted_at: Optional[datetime]
+    
+    # not implemented in the database, but used in the code
+    quota: Optional[int] = 0
