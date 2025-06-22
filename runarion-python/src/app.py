ee06078f6db2b2fbb20fe4260a0d97d413dd6b64
Muhammad Akbar <m.akbar.pro@gmail.com>
2025-06-23 00:33:01 +0700

    ammended initial commit

--- runarion-python/src/app.py
+++ runarion-python/src/app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 from psycopg2 import pool
 from api.generation import generate
-from api.deconstructor import deconstructor
+from api.author_style import author_style
 from api.story_rewrite import story_rewrite
 
 load_dotenv()
@@ -62,7 +62,8 @@ app.config['CONNECTION_POOL'] = connection_pool
 
 
 # --- Upload Location ---
-default_upload_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'uploads'))
+default_upload_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), os.pardir, 'uploads'))
 upload_path = os.getenv('UPLOAD_PATH') or default_upload_path
 os.makedirs(upload_path, exist_ok=True)
 app.config['UPLOAD_PATH'] = upload_path
@@ -71,7 +72,7 @@ app.config['UPLOAD_PATH'] = upload_path
 # --- Blueprint Registration ---
 
 app.register_blueprint(generate, url_prefix='/api')
-app.register_blueprint(deconstructor, url_prefix='/api/deconstructor')
+app.register_blueprint(author_style, url_prefix='/api')
 app.register_blueprint(story_rewrite, url_prefix='/api')
 
 # --- Health Check ---
