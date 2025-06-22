ec3f5827ce230c60096be86b954f3f5f7586c5e6
Pandega Abyan Zumarsyah <pandegaabyan@gmail.com>
2025-06-22 19:59:34 +0700

    Author Style API (#46)
    * add author name in structured author style
    
    * add init files in deconstructor folders and update imports
    
    * update init and query in author style configuration
    
    * update init in paragraph extractor
    
    * add connection pool and upload path as app config
    
    * add deconstructor api
    
    * add uploads folder
    
    * update deconstructor

---
+++ runarion-python/src/services/deconstructor/__init__.py
@@ -0,0 +1,4 @@
+from .author_style_configuration.author_style_configuration import (
+    AuthorStyleConfiguration,
+)
+from .author_style_configuration.paragraph_extractor import ParagraphExtractor
