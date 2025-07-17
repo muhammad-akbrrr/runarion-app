befe888cbfb0b9337f93cf8afe3ce36ffb14ce10
pandega <pandegaabyan@gmail.com>
2025-07-17 14:24:42 +0700

    update init imports

--- runarion-python/src/models/style_analyzer/__init__.py
+++ runarion-python/src/models/style_analyzer/__init__.py
@@ -1,1 +1,7 @@
 from .author_style import AuthorStyle, AuthorStyleExamples, AuthorStyleTechniques
+
+__all__ = [
+    "AuthorStyle",
+    "AuthorStyleExamples",
+    "AuthorStyleTechniques",
+]
--- runarion-python/src/services/style_analyzer/__init__.py
+++ runarion-python/src/services/style_analyzer/__init__.py
@@ -1,6 +1,6 @@
-from orchestrator import StyleAnalyzerOrchestrator
-from stage_1_sampling import SamplingStage
-from stage_2_profiling import ProfilingStage
+from .orchestrator import StyleAnalyzerOrchestrator
+from .stage_1_sampling import SamplingStage
+from .stage_2_profiling import ProfilingStage
 
 __all__ = [
     "StyleAnalyzerOrchestrator",
